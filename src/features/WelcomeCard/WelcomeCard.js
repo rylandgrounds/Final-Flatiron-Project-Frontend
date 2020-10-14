4b68f20ac0defc6679febeddd983941c4dff2925
rylandgrounds <rylandgrounds@gmail.com>
2020-10-14 11:51:38 -0500

    testing removal of sizing

--- src/features/WelcomeCard/WelcomeCard.js
+++ src/features/WelcomeCard/WelcomeCard.js
@@ -7,7 +7,6 @@ import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import { sizing } from '@material-ui/system';
 
 const useStyles = makeStyles({
   root: {
